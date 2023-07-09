cb903d79503b7b15e923e60249f7d1593b9068f8
Shalom biton <shalombiton555@gmail.com>
2023-07-09 12:22:19 +0300

    support stop timer & continue timer

--- src/cmps/timer.jsx
+++ src/cmps/timer.jsx
@@ -1,20 +1,51 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const Timer = ({ time }) => {
-    // optional: get time from cmp 
-    // optimal : get time from props
 
+    const intervalIdRef = useRef()
+    let [count, setCount] = useState(time)
 
     useEffect(() => {
         console.log('time:', time)
+        startInterval()
     }, [])
 
+    useEffect(() => {
+        console.log('count:', count)
+        if (!count) clearInterval(intervalIdRef.current)
+    }, [count])
 
-    console.log('render')
+
+    const startInterval = () => {
+        if (!intervalIdRef.current) {
+        intervalIdRef.current = setInterval(() => {
+            setCount(count => count - 1)
+        }, 1000)
+        }
+
+
+    }
+    const stopTimer = () => {
+        clearInterval(intervalIdRef.current)
+        intervalIdRef.current = ''
+    }
+
+
+    function convertToTime(seconds) {
+        const minutes = Math.floor(seconds / 60) // חילוק השניות לדקות
+        const remainingSeconds = seconds % 60 // השניות היתרות
+
+        const formattedMinutes = minutes.toString().padStart(2, '0') // המרת הדקות לפורמט של שני ספרות
+        const formattedSeconds = remainingSeconds.toString().padStart(2, '0') // המרת השניות לפורמט של שני ספרות
+        return `${formattedMinutes}:${formattedSeconds}`;
+    }
+
     return (
         <section className="timer">
             <h3>Timer</h3>
-
+            time: {convertToTime(count)}
+            <button onClick={stopTimer}>stop timer</button>
+            <button onClick={startInterval} >continue timer</button>
         </section>
     )
 }
